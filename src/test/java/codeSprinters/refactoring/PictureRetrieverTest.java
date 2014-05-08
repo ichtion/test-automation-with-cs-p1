24c378b7c2c0d40f58660d7ae56ccaa594711fc7
unknown <kukula@PLKRNB014.transporeon.nil>
2014-05-08 14:47:57 +0200

    pq changes in names of classes

--- src/test/java/codeSprinters/refactoring/PictureRetrieverTest.java
+++ src/test/java/codeSprinters/refactoring/PictureRetrieverTest.java
@@ -19,45 +19,39 @@ public class PictureRetrieverTest {
     private PictureRetriever pictureRetriever = new PictureRetriever();
 
     @Test(expected = NoLoggedUserException.class)
-    public void test1() throws Exception {
+    public void shouldNotGetPictureIfNotLoggedIn() throws Exception {
         User user1 = new User();
         pictureRetriever.getPicturesFor(user1, null);
     }
 
     @Test
-    public void test2() {
+    public void shouldGetNewPictureIfNewFriend() {
         Picture picture = new Picture();
         User user1 = new User();
         User user2 = new User();
         user2.addFriend(user1);
         PictureDao.addPicturesForUser(user1, Arrays.asList(picture));
-
         List<Picture> list = pictureRetriever.getPicturesFor(user1, user2);
-
         assertThat(list, Matchers.containsInAnyOrder(picture));
     }
 
     @Test
-    public void test3() {
+    public void shouldNotGetPictureIfNotFriend() {
         User user1 = new User();
         User user2 = new User();
-
         List<Picture> list = pictureRetriever.getPicturesFor(user1, user2);
-
         assertThat(list, is(nullValue()));
     }
 
     @Test
-    public void test4() {
+    public void shouldNotGetNewAddedPictureIfNotFriend() {
         Picture picture = new Picture();
         User user1 = new User();
         User user2 = new User();
         User user3 = new User();
         user2.addFriend(user3);
         PictureDao.addPicturesForUser(user1, Arrays.asList(picture));
-
         List<Picture> list = pictureRetriever.getPicturesFor(user1, user2);
-
         assertThat(list, is(nullValue()));
     }
 }
